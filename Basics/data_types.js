538d264cd8f8875a5e82c17cf769ad6b25a3c278
Sowmya Ranjan Behera <90180650+Sowmya-Ranjan-Behera@users.noreply.github.com>
2023-10-06 15:40:20 +0000

    commit it

---
+++ Basics/data_types.js
@@ -0,0 +1,19 @@
+// today we will learn about data types of javascript
+//there are many types of data types
+let age=10 // its a number type of data type
+let myname="Sowmya" // its a string tyoe of data type
+let goal; //its an undefined data type 
+let bool=true // its an boolean type of data type
+
+let tommorrow=null // actually its type is defined as null its an standalone datatype.
+                    // if haven't assigned anything to a variable and we want to occupy its sapce we can use null
+// symbol is an unique data type
+//suppose we want to use integer but that is greater than the range of normal integer then we use big Integer
+
+
+//lets see the output of all these variables what types pf datatypes are they
+
+console.table(["age is " +typeof(age),"name is "+ typeof(myname),"goal is "+typeof(goal)+" and bool is "+typeof(bool)])
+
+
+//another datatypes are obejcts
