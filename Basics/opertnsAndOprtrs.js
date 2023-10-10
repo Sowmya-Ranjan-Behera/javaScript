90278e52480bbf1bb96f78e1d8030096619af541
Sowmya Ranjan Behera <90180650+Sowmya-Ranjan-Behera@users.noreply.github.com>
2023-10-10 19:39:43 +0000

    conversion

---
+++ Basics/opertnsAndOprtrs.js
@@ -0,0 +1,18 @@
+// so like every other language javascript also have same operation and operators
+// you can add multiply subtract divide and find the modulo
+// how ever using same data type there wont be any problem but using diffrent data type lets see
+
+// console.log("2"+1);// it will be "21 " because after string it treats all the numbers as string in addition
+//console.log("21"*2);// but all other operators will just perform the mathematical operations
+//console.log(true+1);// true will be converted to 1 samewise false will be converted to 0
+//console.log(true+"");// this will be converted to string
+
+//*************************************comparision operator*********************** */
+
+//for same datatype its easy to compare but for diffrent datatype it will less complicated
+
+//console.log("21">43);// string will be treated as number
+//console.log("a" < 21); // this line will return false
+//console.log("s">"ta"); // ascii code is checked in here
+//console.log(undefined>=1);// cant be evaluated
+//by the way writing code like this will only make you have bad impression
