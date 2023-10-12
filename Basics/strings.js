2562cf129bce0438ced475b2f9f66b3f11fe675c
@Sowmya-Ranjan-Behera <amsowmyaranjanb@gmail.com>
2023-10-12 18:53:30 +0530

    string Manipulation

---
+++ Basics/strings.js
@@ -0,0 +1,17 @@
+// so we learn about the Strings here
+//String are the secunce of characters 
+// creation of string 
+let a = String("Hello");
+console.log(a);
+console.log(a.to);
+// so string is specially known for its functions
+// so lets look at some functions 
+// function name           ===>             work
+
+//  length                             to find the length of the string 
+//  toUpperCase                         converts the string to upper case 
+//  trim                                to remove the whitespaces(two more methods of trim is there trimstart and trimend)
+//  substring                           to find the substring with given Initial and final index but final index is not counted
+//  slice                               to slice the string into smaller parts and it can take negative arguments also
+//  concat                              To concat one string with another
+//  replace                             replace the string with desired character
