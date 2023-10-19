9652bd1cb3c7835a44ef6d5227f60877a732c904
@Sowmya-Ranjan-Behera <amsowmyaranjanb@gmail.com>
2023-10-20 01:46:25 +0530

    math

---
+++ Basics/mathClass.js
@@ -0,0 +1,18 @@
+// in thjs chapter we will know about math classes
+// so basically Math is a global class having different functions that make our life easy in daily 
+// life programming
+const num = 29.2342323;
+console.log(Math.ceil(num));
+
+
+// so there are various methods that can be used to calculate differnt mathematic operations
+//      function   =>       work
+//      round               to round up the value upto nearest number
+//      floor               convert the float value into integer type
+//      toFixed             change the large decimal value upto desired decimal places
+//      ceil                used to round the next value
+//      abs                 converts the negative number to a positive only
+//      sqrt                funtions is used to calculate the square root of the number
+//      pow                 functions is used to calculate the power of the number 
+//      min                 to find the min of the number
+//      max                 to find the max of the number
