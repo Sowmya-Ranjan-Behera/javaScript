131b0b349ff451d407543710e7fcffdf57d4fe41
@Sowmya-Ranjan-Behera <amsowmyaranjanb@gmail.com>
2023-12-03 23:53:56 +0530

    armstring

--- 100Questions/amstrong.js
+++ 100Questions/amstrong.js
@@ -2,7 +2,7 @@ let armstrong = parseInt(prompt("Enter the number to check : "));
 let length = Math.floor(Math.log10(armstrong)+1);
 console.log(length);
 let copy = armstrong;
-let temp=0;
+let temp=0; 
 while(length>0){
     length--;
     temp=((copy%10)*Math.pow(10,length) )+temp;
